2f57b0d9b305925e51d14908d66b295b9095997e
Gregory Sharov <sharov.grigory@gmail.com>
2020-08-07 11:20:57 +0100

    3.1.3

--- spider/constants.py
+++ spider/constants.py
@@ -26,7 +26,7 @@
 
 # Define the version of the 'spider' Python module, that can also be
 # imported from setup.py for PyPI distribution
-__version__ = '3.1.2'
+__version__ = '3.1.3'
 
 # ----------------- Constants values --------------------------------------
 SPIDER_HOME = 'SPIDER_HOME'
