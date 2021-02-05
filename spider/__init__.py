d651866511732e826fb7af3f854024e0f789b176
Grigory Sharov <sharov.grigory@gmail.com>
2021-02-05 22:47:33 +0000

    fix use of exists and join

--- spider/__init__.py
+++ spider/__init__.py
@@ -27,7 +27,7 @@
 import os
 
 import pwem
-from pyworkflow.utils import Environ, join
+from pyworkflow.utils import Environ
 
 from .constants import *
 
@@ -74,7 +74,7 @@ class Plugin(pwem.Plugin):
     @classmethod
     def getScript(cls, *paths):
         """ Return the script that will be used. """
-        cmd = join(__path__[0], 'scripts', cls.getActiveVersion(), *paths)
+        cmd = os.path.join(__path__[0], 'scripts', cls.getActiveVersion(), *paths)
         return str(cmd)
 
     @classmethod
@@ -84,7 +84,7 @@ class Plugin(pwem.Plugin):
         else:
             program = os.path.basename(cls.getVar(SPIDER))
 
-        cmd = os.path.abspath(join(cls.getEnviron()[SPBIN_DIR], program))
+        cmd = os.path.abspath(os.path.join(cls.getEnviron()[SPBIN_DIR], program))
 
         return str(cmd)
 
