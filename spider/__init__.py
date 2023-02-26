96fca1cbadef0362b50e912d2620a8050c4616b6
Grigory Sharov <sharov.grigory@gmail.com>
2023-02-26 12:38:24 +0000

    fix plugin url

--- spider/__init__.py
+++ spider/__init__.py
@@ -42,7 +42,7 @@ class Plugin(pwem.Plugin):
     _homeVar = SPIDER_HOME
     _pathVars = [SPIDER_HOME]
     _supportedVersions = ['26.06']
-    _url = "https://github.com/spider-em/SPIDER"
+    _url = "https://github.com/scipion-em/scipion-em-spider"
 
     @classmethod
     def _defineVariables(cls):
